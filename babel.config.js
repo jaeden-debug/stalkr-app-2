5fbb18f114ffb8b5a874ed4d82bd6f79cc99734a
Jaeden Doody <subscriptions@stillawakemedia.com>
2026-05-28 19:13:35 -0500

    Temporarily disable NativeWind Babel preset for SDK 51

--- babel.config.js
+++ babel.config.js
@@ -1,28 +1,10 @@
 module.exports = function (api) {
   api.cache(true);
+
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
     plugins: [
-      'nativewind/babel',
-      'react-native-reanimated/plugin',
-      [
-        'module-resolver',
-        {
-          root: ['./'],
-          alias: {
-            '@': './src',
-            '@app': './app',
-            '@components': './src/components',
-            '@hooks': './src/hooks',
-            '@services': './src/services',
-            '@store': './src/store',
-            '@types': './src/types',
-            '@utils': './src/utils',
-            '@constants': './src/constants',
-            '@config': './src/config',
-          },
-        },
-      ],
-    ],
+      "react-native-reanimated/plugin"
+    ]
   };
 };
